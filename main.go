023876c01b8478a01dbb18005940c893c4f40706
viktor-kurchenko <viktor.kurchenko@isovalent.com>
2024-07-03 20:45:46 +0300

    hmac functions.
    Signed-off-by: viktor-kurchenko <viktor.kurchenko@isovalent.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
 	"hash"
@@ -14,24 +15,35 @@ type hashFn struct {
 	fn   func() hash.Hash
 }
 
+var hashFns = []hashFn{
+	{name: "SHA-224", fn: sha256.New224},
+	{name: "SHA-256", fn: sha256.New},
+	{name: "SHA3-224", fn: sha3.New224},
+	{name: "SHA3-256", fn: sha3.New256},
+	{name: "SHA3-384", fn: sha3.New384},
+	{name: "Whirlpool", fn: whirlpool.New},
+}
+
 func main() {
 	phrase := []byte("hello")
+	key := []byte("cryptography")
 
 	printHashes(phrase)
+	printHMACs(key, phrase)
 }
 
 func printHashes(phrase []byte) {
-	hashFns := []hashFn{
-		{name: "SHA-224", fn: sha256.New224},
-		{name: "SHA-256", fn: sha256.New},
-		{name: "SHA3-224", fn: sha3.New224},
-		{name: "SHA3-256", fn: sha3.New256},
-		{name: "SHA3-384", fn: sha3.New384},
-		{name: "Whirlpool", fn: whirlpool.New},
-	}
 	for i := range hashFns {
 		h := hashFns[i].fn()
 		h.Write(phrase)
 		fmt.Printf("%s: %x\n", hashFns[i].name, h.Sum(nil))
+	}
+}
+
+func printHMACs(key, phrase []byte) {
+	for i := range hashFns {
+		h := hmac.New(hashFns[i].fn, key)
+		h.Write(phrase)
+		fmt.Printf("HMAC-%s: %x\n", hashFns[i].name, h.Sum(nil))
 	}
 }
